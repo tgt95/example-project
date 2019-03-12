506683235192e9a329cb51206d59bb24a1f4a03b
Trinh Gia Tuan <tuan.trinh@bigin.vn>
2019-03-12 16:23:56 +0700

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# example-project
+# Example Project includes Build System (Gulp)
+
+1. Compile SCSS:
+	- Build: scss
+	- Watch: scss:watch
+
+2. Compile JS:
+	- Build: js
+
+3. Watch CSS & JS:
+	- Build: watch
---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+var autoprefixer = require('autoprefixer');
+var gulp     	 = require('gulp');
+var cleanCss 	 = require('gulp-clean-css');
+var postcss      = require('gulp-postcss');
+var sass 		 = require('gulp-sass');
+var sourcemaps   = require('gulp-sourcemaps');
+var rename 		 = require('gulp-rename');
+var uglify 		 = require('gulp-uglify');
+
+
+/**
+1. Compile SCSS:
+	- Build: scss
+	- Watch: scss:watch
+
+2. Compile JS:
+	- Build: js
+
+3. Watch CSS & JS:
+	- Build: watch
+*/
+
+var paths = {
+	src: {
+		scss : 'src/scss',
+		js : 'src/js'
+	},
+	dist: {
+		css : 'assets/css',
+		js : 'assets/js'
+	}
+}
+
+/* Compile SCSS */
+gulp.task('scss', () => {
+	return gulp.src(paths.src.scss + '/*.scss')
+	.pipe(sourcemaps.init())
+	.pipe(sass().on('error', sass.logError))
+	.pipe(postcss([ autoprefixer({
+        browsers: ["> 0.3%", "last 7 versions", "Android >= 4", "Firefox >= 20", "iOS >= 8"]
+    }) ]))
+	.pipe(gulp.dest(paths.dist.css))
+    .pipe(rename({ suffix: '.min' }))
+	.pipe(cleanCss({level: {1: {specialComments: 0}}}))
+	.pipe(sourcemaps.write('./'))
+	.pipe(gulp.dest(paths.dist.css))
+});
+
+gulp.task('scss:watch', ['scss'], () => {
+	gulp.watch(paths.src.scss + '/*.scss', ['scss']);
+});
+
+
+/* Compile JS */
+gulp.task('js', () => {
+	return gulp.src([
+            // 'Your URL',
+            paths.src.js + '/main.js'
+		])
+	.pipe(uglify())
+	.pipe(rename({ suffix: '.min' }))
+	.pipe(gulp.dest(paths.dist.js))
+});
+
+gulp.task('js:watch', ['js'], () => {
+	gulp.watch(paths.src.js + '/main.js', ['js']);
+});
+
+/* Watch All */
+gulp.task('watch', ['scss', 'js'], () => {
+	gulp.watch(paths.src.scss + '/*.scss', ['scss']);
+	gulp.watch(paths.src.js + '/main.js', ['js']);
+});
